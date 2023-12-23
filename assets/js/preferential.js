ac8af5c1e3cf98875595e4b26b38c8670a4c75fa
Đỗ Hùng <dohung2ka3@gmail.com>
2023-12-23 18:33:09 +0700

    create preferential file and media query

---
+++ assets/js/preferential.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const navbar = document.querySelector("[data-navbar]");
+const menuOpen = document.querySelector("[data-menu-open]");
+const menuClose = document.querySelector("[data-menu-close]");
+const overlay = document.querySelector("[data-overlay]");
+
+const cart = document.querySelector("[data-cart]");
+const cartList = document.querySelector("[data-cart-list]");
+const cartClose = document.querySelector("[data-cart-close]");
+
+const navElems = [menuOpen, menuClose, overlay];
+const cartElems = [cart, cartClose];
+
+for (let i = 0; i < navElems.length; i++) {
+    navElems[i].addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+}
+
+for (let i = 0; i < cartElems.length; i++) {
+    cartElems[i].addEventListener("click", function () {
+        cartList.classList.toggle("active");
+    });
+}
